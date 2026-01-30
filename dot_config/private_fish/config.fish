5ab69df436bbc177785f6c6938fdc6baf5d196c3
vladg00dman <vladislavsunny@gmail.com>
2026-01-30 08:35:14 +0300

    waybar

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     starship init fish | source
 end
 
+if status is-login
+    fish_add_path ~/.local/bin
+end
+
 alias ls="exa --icons"
 alias ll="exa -la --icons"
 alias cat="bat"
