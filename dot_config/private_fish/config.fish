8ced37c1c68922830c4023defe578177d8be7e80
vladg00dman <vladislavsunny@gmail.com>
2026-01-30 10:33:32 +0300

    mise for node js

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,6 +6,10 @@ if status is-login
     fish_add_path ~/.local/bin
 end
 
+if type -q mise
+    mise activate fish | source
+end
+
 alias ls="exa --icons"
 alias ll="exa -la --icons"
 alias cat="bat"
